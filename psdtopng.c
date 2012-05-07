c573b4ac1d5314cd3ea391f2d892443e6b6f49eb
Swyter <Swyter@Portatil>
2012-05-07 16:55:45 +0200

    *Consistent style, tabs to spaces and EOL cleanup...

--- psdtopng.c
+++ psdtopng.c
@@ -1,7 +1,7 @@
 /*  -----------------------------------------------
  *  © 2012 Created by Swyter <swyterzone@gmail.com>
  *  Licensed under MIT-like terms <http://opensource.org/licenses/mit-license.php>
- 
+
  *  Uses stb_image, by Sean Barrett and contributors. :)
  */
 
@@ -22,10 +22,10 @@ int main(int argc, char **argv)
 {
     if(argc < 2)
     {
-		//get the app name
-		char *exec = strrchr(argv[0], '\\');
-		if(!exec) exec=argv[0]; else exec+=1;
-	
+        //get the app name
+        char *exec = strrchr(argv[0], '\\');
+        if(!exec) exec=argv[0]; else exec+=1;
+
         printf("PSD, GIF, JPG, TGA, PIC -> PNG -- By Swyter\n");
         printf("Usage: %s <input filename> <optional output filename>\n", exec);
         system("pause");
@@ -34,29 +34,29 @@ int main(int argc, char **argv)
     else if(argc == 2)
     {
         static char * ext = ".png";
-        
+
         argv[2] = malloc(strlen(argv[1]) + strlen(ext));
         strcpy(argv[2], argv[1]);
         strcat(argv[2], ext);
     }
     //printf("%s\n%s\n%s\n",argv[0],argv[1],argv[2]);
     printf("%s -> %s\n",argv[1],argv[2]);
-    
+
     int x, y, comp;
     unsigned char *foo;
-    
+
         foo =      stbi_load(argv[1], &x, &y, &comp, 0);
-    int img = stbi_write_png(argv[2],  x,  y,  comp, foo, 0); 
-    
-    //sprintf(cVal,"%f",fVal); 
-    
+    int img = stbi_write_png(argv[2],  x,  y,  comp, foo, 0);
+
+    //sprintf(cVal,"%f",fVal);
+
     if(img!=0){
         printf("[!] image creation successful\n");
     }else{
         printf("[!] meh, doesn't works!\n");
     }
-    
+
     stbi_image_free(foo);
-    
+
     return img;
 }
